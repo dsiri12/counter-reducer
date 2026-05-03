47032ce34772b3594950a5d3ceac16dcaf649da3
Damien Siri <dsiri12@gmail.com>
2026-05-03 16:06:24 -0400

    add text to state

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,28 @@
-import { useReducer } from "react";
+import { useReducer, type ChangeEvent } from "react";
 
-const initState = { count: 0 };
+const initState = { count: 0, text: "" };
 
 type CounterState = typeof initState;
 
-type REDUCER_ACTION_TYPE = "INCREMENT" | "DECREMENT";
-// const enum REDUCER_ACTION_TYPE {
-//   INCREMENT,
-//   DECREMENT,
-// }
+type REDUCER_ACTION_TYPE = "INCREMENT" | "DECREMENT" | "NEW_INPUT";
 
+// shape for dispatch param object
 type ReducerAction = {
   type: REDUCER_ACTION_TYPE;
+  payload?: string;
 };
 
 const reducer = (state: CounterState, action: ReducerAction): CounterState => {
   switch (action.type) {
     case "INCREMENT":
       return { ...state, count: state.count + 1 };
+
     case "DECREMENT":
       return { ...state, count: state.count - 1 };
+
+    case "NEW_INPUT":
+      return { ...state, text: action.payload };
+
     default:
       throw new Error(`oop! wrong action type: ${action.type}`);
   }
@@ -31,6 +34,13 @@ export const Counter = () => {
   const increment = () => dispatch({ type: "INCREMENT" });
   const decrement = () => dispatch({ type: "DECREMENT" });
 
+  const handleTextInput = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "NEW_INPUT",
+      payload: event.target.value,
+    });
+  };
+
   return (
     <>
       <h1>Current Count: {state.count}</h1>
@@ -39,6 +49,10 @@ export const Counter = () => {
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
       </div>
+
+      <input type="text" onChange={handleTextInput} />
+
+      <h3>{`Input text: ${state.text}`}</h3>
     </>
   );
 };
